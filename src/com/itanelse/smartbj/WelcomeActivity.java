64a1c8fc7f066413d5b903db08aff6ce40f76cef
AnElse <anelse@outlook.com>
2015-11-29 08:59:40 +0800

    初始化了代码规范和版本控制规范

---
+++ src/com/itanelse/smartbj/WelcomeActivity.java
@@ -0,0 +1,29 @@
+package com.itanelse.smartbj;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * 
+ * @项目名称: SmartBJ
+ * @包名: com.itanelse.smartbj
+ * @类名: SplashActivity
+ * @时间: 上午8:53:44
+ * @作者: AnElse
+ * 
+ * @描述: 欢迎界面的实现
+ *
+ * @当前版本号: $Rev: 4 $
+ * @更新人: $Author: vaio $
+ * @更新的时间: $date$
+ * @更新的描述: TODO
+ *
+ */
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+}
